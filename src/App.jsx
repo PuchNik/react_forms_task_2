6b588d779af198aee7b30a49f23eae9a4ce19570
PuchNik <i.p.chn.k@gmail.com>
2025-04-01 22:03:36 +0300

    Completed 100%

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ const sendFormData = (formData) => {
 
 function App() {
   const buttonRef = useRef(null)
-  const [isSubmitted, setIsSubmitted] = useState(false)
   const [touchedFields, setTouchedFields] = useState({
     email: false,
     password: false,
     confirmPassword: false,
   })
+
+  const isSubmitted = false
+  const setIsSubmitted = () => !isSubmitted
 
   const {
     register,
@@ -29,7 +31,7 @@ function App() {
   })
 
   const onSubmit = (data) => {
-    setIsSubmitted(true)
+    setIsSubmitted()
     sendFormData(data)
   }
 
@@ -44,60 +46,63 @@ function App() {
   }, [isValid])
 
   return (
-      <div className={styles['registration-form']}>
-        <h2>Регистрация</h2>
-        <form className={styles['registration-form']} onSubmit={handleSubmit(onSubmit)}>
-          <div className={styles['form-group']}>
-            <input
-                id="email"
-                type="email"
-                placeholder="E-mail"
-                {...register('email')}
-                onBlur={() => handleBlur('email')}
-                aria-invalid={errors.email ? 'true' : 'false'}
-            />
-            {touchedFields.email && errors.email && (
-                <div className={styles['error']}>{errors.email.message}</div>
-            )}
-          </div>
-          <div className={styles['form-group']}>
-            <input
-                id="password"
-                type="password"
-                placeholder="Password"
-                {...register('password')}
-                onBlur={() => handleBlur('password')}
-                aria-invalid={errors.password ? 'true' : 'false'}
-            />
-            {touchedFields.password && errors.password && (
-                <div className={styles['error']}>{errors.password.message}</div>
-            )}
-          </div>
-          <div className={styles['form-group']}>
-            <input
-                id="confirmPassword"
-                type="password"
-                placeholder="Repeat password"
-                {...register('confirmPassword')}
-                onBlur={() => handleBlur('confirmPassword')}
-                aria-invalid={errors.confirmPassword ? 'true' : 'false'}
-            />
-            {touchedFields.confirmPassword && errors.confirmPassword && (
-                <div className={styles['error']}>
-                  {errors.confirmPassword.message}
-                </div>
-            )}
-          </div>
-          <button
-              type="submit"
-              ref={buttonRef}
-              disabled={!isValid}
-              className={styles['submit-button']}
-          >
-            Зарегистрироваться
-          </button>
-        </form>
-      </div>
+    <div className={styles['registration-form']}>
+      <h2>Регистрация</h2>
+      <form
+        className={styles['registration-form']}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className={styles['form-group']}>
+          <input
+            id="email"
+            type="email"
+            placeholder="E-mail"
+            {...register('email')}
+            onBlur={() => handleBlur('email')}
+            aria-invalid={errors.email ? 'true' : 'false'}
+          />
+          {touchedFields.email && errors.email && (
+            <div className={styles['error']}>{errors.email.message}</div>
+          )}
+        </div>
+        <div className={styles['form-group']}>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            {...register('password')}
+            onBlur={() => handleBlur('password')}
+            aria-invalid={errors.password ? 'true' : 'false'}
+          />
+          {touchedFields.password && errors.password && (
+            <div className={styles['error']}>{errors.password.message}</div>
+          )}
+        </div>
+        <div className={styles['form-group']}>
+          <input
+            id="confirmPassword"
+            type="password"
+            placeholder="Repeat password"
+            {...register('confirmPassword')}
+            onBlur={() => handleBlur('confirmPassword')}
+            aria-invalid={errors.confirmPassword ? 'true' : 'false'}
+          />
+          {touchedFields.confirmPassword && errors.confirmPassword && (
+            <div className={styles['error']}>
+              {errors.confirmPassword.message}
+            </div>
+          )}
+        </div>
+        <button
+          type="submit"
+          ref={buttonRef}
+          disabled={!isValid}
+          className={styles['submit-button']}
+        >
+          Зарегистрироваться
+        </button>
+      </form>
+    </div>
   )
 }
 
